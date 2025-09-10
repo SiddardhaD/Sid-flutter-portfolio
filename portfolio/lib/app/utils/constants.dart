3aacc1fd911c172c9b01a9c1905046853faad540
SiddardhaD-at-194563677010 <sdevarayapalli@krispykreme.com>
2025-09-10 21:02:21 +0530

    Sidebar
    Fully UI Sidebar

--- portfolio/lib/app/utils/constants.dart
+++ portfolio/lib/app/utils/constants.dart
@@ -3,7 +3,26 @@ import 'dart:ui';
 class AppConstats {
   //Images
   static const profile = "assets/images/profile.png";
+  static const linkedin = "assets/images/linkedin.png";
+  static const github = "assets/images/github.png";
 
   //Colors
-  static const Color sidebarBackGround = Color(0XFF1E1E1F);
+  static const Color eerieBlack = Color(0XFF1E1E1F);
+  static const Color onyx = Color(0XFF383838);
+  static const Color neonGreen = Color(0XFF3AFF16);
+  static const Color lotion = Color(0XFFFAFAFA);
+  static const Color raisinBlack = Color(0XFF202022);
+  static const Color blackOlive = Color(0XFF3A3A3A);
+
+  static const Color quickSilver = Color(0XFF9F9F9F);
+
+  //Strings
+  static const String username = "Siddardha Devarayapalli";
+  static const String userRole = "Flutter Developer";
+  static const String userEmail = "Siddardhadevarayapalli@gmail.com";
+  static const String userphone = "+91 9985091823";
+  static const String userlocation = "Hyderabad, Telanagana";
+  static const String email = "EMAIL";
+  static const String phone = "PHONE";
+  static const String location = "LOCATION";
 }
--- portfolio/lib/app/widgets/sidebar.dart
+++ portfolio/lib/app/widgets/sidebar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/app/utils/constants.dart';
+import 'package:portfolio/app/utils/styles.dart';
 
 class SideBar extends StatelessWidget {
   const SideBar({super.key});
@@ -8,59 +9,183 @@ class SideBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Container(
-      width: screenSize.width / 3,
+      width: screenSize.width / 5,
       height: screenSize.height / 1.2,
-      color: AppConstats.sidebarBackGround,
+      decoration: BoxDecoration(
+        color: AppConstats.eerieBlack,
+        border: Border.all(color: AppConstats.onyx, width: 2),
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+      ),
       margin: const EdgeInsets.all(20),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage(
-              AppConstats.profile,
-            ), // add your image in assets
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            "Siddardha Devarayapalli",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width / 20,
+            ),
+            decoration: BoxDecoration(
+              color: AppConstats.onyx,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Image.asset(AppConstats.profile),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      color: AppConstats.neonGreen,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+          const SizedBox(height: 20),
+          Text(AppConstats.username, style: TextStyling().userName),
           const SizedBox(height: 5),
-          const Text("Flutter Developer", style: TextStyle(color: Colors.grey)),
-          const Divider(height: 40, color: Colors.grey),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: AppConstats.onyx,
+            ),
+            child: Text(AppConstats.userRole, style: TextStyling().userRole),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.02),
+            child: Divider(height: 40, color: AppConstats.onyx),
+          ),
 
-          Row(
-            children: const [
-              Icon(Icons.email, color: Colors.amber),
-              SizedBox(width: 10),
-              Text(
-                "youremail@example.com",
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ProfileDetailCard(
+                  title: AppConstats.email,
+                  icon: Icons.email,
+                  label: AppConstats.userEmail,
+                ),
+                SizedBox(height: 10),
+                ProfileDetailCard(
+                  title: AppConstats.phone,
+                  icon: Icons.phone,
+                  label: AppConstats.userphone,
+                ),
+                SizedBox(height: 10),
+                ProfileDetailCard(
+                  title: AppConstats.location,
+                  icon: Icons.location_on,
+                  label: AppConstats.userlocation,
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 15),
-          Row(
-            children: const [
-              Icon(Icons.phone, color: Colors.amber),
-              SizedBox(width: 10),
-              Text("+91 9876543210", style: TextStyle(color: Colors.white)),
-            ],
-          ),
-          const SizedBox(height: 15),
-          Row(
-            children: const [
-              Icon(Icons.location_on, color: Colors.amber),
-              SizedBox(width: 10),
-              Text("Hyderabad, India", style: TextStyle(color: Colors.white)),
-            ],
+          SizedBox(height: screenSize.height / 12),
+          SizedBox(
+            width: screenSize.width / 16,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {
+                    debugPrint("Github");
+                  },
+                  child: SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: Image.asset(
+                      AppConstats.github,
+                      color: AppConstats.quickSilver,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    debugPrint("Linkedin");
+                  },
+                  child: SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: Image.asset(
+                      AppConstats.linkedin,
+                      color: AppConstats.quickSilver,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class ProfileDetailCard extends StatelessWidget {
+  final String title;
+  final String label;
+  final IconData icon;
+  const ProfileDetailCard({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Row(
+      children: [
+        Container(
+          height: screenSize.width * 0.025,
+          width: screenSize.width * 0.025,
+          decoration: BoxDecoration(
+            color: AppConstats.raisinBlack,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: AppConstats.blackOlive.withOpacity(0.6),
+                offset: const Offset(-6, -6),
+                blurRadius: 10,
+              ),
+              BoxShadow(
+                color: AppConstats.blackOlive.withOpacity(0.1),
+                offset: const Offset(0, 0),
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Icon(
+              icon,
+              color: Colors.amber,
+              size: screenSize.width * 0.012,
+            ),
+          ),
+        ),
+
+        SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title, style: TextStyling().siderheading),
+            Text(label, style: TextStyling().siderAnswer),
+          ],
+        ),
+      ],
     );
   }
 }
